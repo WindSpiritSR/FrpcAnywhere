ba9202f6d4873bf7c7326ad60d6251030580598d
WindSpirit <simon343riley@gmail.com>
2021-04-30 09:56:47 +0800

    Fix log?

--- app/src/main/java/cn/leviatan/frpcanywhere/MainActivity.kt
+++ app/src/main/java/cn/leviatan/frpcanywhere/MainActivity.kt
@@ -22,7 +22,8 @@ import java.util.*
 import kotlin.concurrent.thread
 
 class MainActivity : ComponentActivity() {
-    private var logStr by mutableStateOf(arrayListOf(""))
+    private var logStrArray = arrayListOf<String>()
+    private var logStr by mutableStateOf("")
     private var maxLogLines = 5
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -72,8 +73,8 @@ class MainActivity : ComponentActivity() {
 
     private fun runShowLog() {
         val startTime = SimpleDateFormat("MM-dd HH:mm:ss.SSS").format(Date())
-        println("============================================================ $startTime")
-        val logcatCmd = arrayOf("logcat", "GoLog:I", "*:S", "-v", "raw", "-t", startTime)
+        println("============================================================ Catch Logcat at [$startTime]")
+        val logcatCmd = arrayOf("logcat", "GoLog:I", "-s", "-v", "raw")
         val buf = BufferedReader(InputStreamReader(Runtime.getRuntime().exec(logcatCmd).inputStream))
         var bufLine: String?
 
@@ -82,14 +83,10 @@ class MainActivity : ComponentActivity() {
         }
 
         while(true) {
-//            FIXME: buf.readLine() always null
             if (buf.readLine().also { bufLine = it } != null) {
-//                if (logStr.size == maxLogLines) {
-//                    logStr.removeFirst()
-//                }
-
-                logStr.add(bufLine!!.replace("\\x1b\\[[0-9;]*m".toRegex(), "") + "\n")
-                println("=================================================== ${logStr.last()}")
+//                FIXME: Only the last log???
+                logStr = bufLine!!.replace("\\x1b\\[[0-9;]*m".toRegex(), "")
+                println("=================================================== Log: [$logStr]")
             }
         }
     }
@@ -97,7 +94,11 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun LogArea() {
         Column {
-            logStr.forEach {
+            if (logStrArray.size == maxLogLines) {
+                logStrArray.removeFirst()
+            }
+            logStrArray.add(logStr)
+            logStrArray.forEach {
                 Text(text = it)
             }
         }
